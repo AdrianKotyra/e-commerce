f1271f303c3915be5c8aef1ea4392cbce65afe50
Adrian <adriankotyra@yahoo.com>
2025-03-05 21:56:06 +0000

    update

---
+++ public/event.php
@@ -0,0 +1,72 @@
+<?php include("includes/header.php") ?>
+
+<section class="hero-section event_hero">
+    <div class="hero-container">
+
+      <h1 class="category_header">Sneaker of the Year</h1>
+      <video type="video/mp4" autoplay loop muted width="100%"height="100%"  src="./videos/sneaker_year/sneaker_year.mp4"></video>
+
+    </div>
+
+
+
+
+</section>
+<section class="sneaker_year_section">
+<div class="event-section section wrapper-extra sneaker_year_col">
+
+<h3 class="section-header">
+Converse Chuck Taylor All Star Hi
+
+</h3>
+<div class="event-section-container">
+
+    <img src="./imgs/products/Converse Chuck Taylor All Star Hi/img1.jpg"  />
+
+
+</div>
+
+
+</div>
+<div class="event-section section wrapper-extra sneaker_year_col">
+<div class="event-section-container">
+    <div class="img-container-event ">
+    <img src="./imgs/products/Converse Chuck Taylor All Star Hi/img2.jpg"  />
+    </div>
+    <div class="event-desc-container flex-col">
+        <div class="event-desc-details">
+            <h3><b>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</b></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam quasi sit vel, rerum dolores enim veniam quisquam voluptatibus, atque, cupiditate repellendus.</p>
+
+
+        </div>
+
+
+    </div>
+
+</div>
+</div>
+
+<div class="event-section section wrapper-extra sneaker_year_col">
+<div class="event-section-container2">
+    <div class="img-container-event ">
+    <img src="./imgs/products/Converse Chuck Taylor All Star Hi/img3.jpg"  />
+    </div>
+    <div class="event-desc-container flex-col">
+        <div class="event-desc-details">
+            <h3><b>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</b></h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laboriosam quasi sit vel, rerum dolores enim veniam quisquam voluptatibus, atque, cupiditate repellendus.</p>
+
+
+        </div>
+
+
+    </div>
+
+</div>
+</div>
+
+</section>
+
+
+<?php include("includes/footer.php") ?>
--- public/includes/sections/event-section.php
+++ public/includes/sections/event-section.php
@@ -1,11 +1,11 @@
-<section class="event-section">
+<section class="event-section wrapper">
     <div class="event-section-container">
         <div class="img-container-event">
             <img src="./imgs/event_section/event.jpg"  />
         </div>
         <div class="event-desc-container flex-col">
             <div class="event-desc-details">
-                <h3>"Don’t miss out on our latest event!</h3>
+                <h3><b>Sneaker of the Year!</b></h3>
                 <p>Explore the Limited Edition Collectibles and step into the season with fresh looks that stand out. Shop now!"</p>
                 <a href="event.php">
                     <button class="button-custom">Check</button>
